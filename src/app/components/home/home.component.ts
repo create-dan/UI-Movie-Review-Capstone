bea0a03ea26cb8ca0f037d7d12313adae26e5685
Dnyaneshwar Shinde <dnyaneshwar.shinde@ideas.com>
2024-09-26 16:49:30 +0530

    Dnyaneshwar | Updated home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,9 +9,35 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HomeComponent {
 
+  images: string[] = [
+    '../../../assets/joker.jpg',
+    '../../../assets/home3.jpg',
+    '../../../assets/home1.jpg',
+     '../../../assets/home5.jpg',
+     '../../../assets/home6.jpg',
+     '../../../assets/home7.jpg',
+  ];
+  
+  currentImageIndex: number = 0;
+  backgroundImage!: string;
+
+  ngOnInit() {
+    this.backgroundImage = this.images[this.currentImageIndex];
+    this.startImageRotation();
+  }
+
+  startImageRotation() {
+    setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+      this.backgroundImage = this.images[this.currentImageIndex];
+    }, 2000); // Change image every 5 seconds
+  }
+
   loginSuccess!:boolean;
 
   constructor(private route: ActivatedRoute) {}
+
+
 
         
 }
